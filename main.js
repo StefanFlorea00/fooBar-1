d69ee4275d625d984227ee5340998a82323b3a17
Stefan <stefflr@yahoo.com>
2020-11-27 11:51:18 +0100

    created db/added parcel/import-export

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+import * as db from "./modules/db.mjs";
+import { rootData } from "./modules/db.mjs";
+
+//***IF YOU TRY TO GET DATA FROM DB AT START OF PAGE */
+//***IT WILL BE EMPTY BECAUSE IT TRIES TO GET DATA BEFORE FETCHING */
+
+
+
+db.get();
+// setTimeout(() => {
+console.log(db.getTaps());  
+// }, 100);
