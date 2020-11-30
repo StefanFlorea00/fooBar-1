67d72407e80327b82e8722bb52eff22161215e24
Andrei Mihutoni <andrei.mihutoni@gmail.com>
2020-11-30 21:39:35 +0100

    adding the level bar funtctionality

--- scripts/taps.js
+++ scripts/taps.js
@@ -4,31 +4,41 @@ import * as db from "../modules/db.mjs";
 db.get();
 setTimeout(() => {
     init();
-}, 100);
+}, 200);
 
-function init(tap) {
+function init() {
     tapsAddBeerNames();
-
+    tapsLevel();
 }
 
 
 
-function tapsAddBeerNames(tap) {
+function tapsAddBeerNames() { //takes the names from JSON data and add them to each tap
 
     let tapsArray = db.getTaps();
-    console.log(tapsArray)
+    // console.log(tapsArray)
     const template = document.querySelector("#tap-template").content;
 
     for (let i = 0; i < tapsArray.length; i++) {
         const clone = template.cloneNode(true);
         clone.querySelector(".beer-name").textContent = tapsArray[i].beer;
-
         document.querySelector(".wrapper-taps").appendChild(clone);
     }
-    // let tap1Beer = tapsArray[0].beer;
-    // console.log(tap1Beer)
+
+
 
 
 
 }
 
+function tapsLevel() { //takes the tap level from JSON data and fill the level bar
+    let tapsLevel = db.getTaps();
+    const template = document.querySelector("#tap-template").content;
+
+    for (let i = 0; i < tapsLevel.length; i++) {
+        const clone = template.cloneNode(true);
+        document.querySelector(".tap-levelbar-fill").style.height = (tapsLevel[i].level / 2500) * 100 + "%"; //sets the height of the fill div in relation to the level
+        console.log(tapsLevel[i].beer, tapsLevel[i].level)
+
+    }
+}
